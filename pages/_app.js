c9bc8304a2a14c96edc5a989ab2f43bc4d51246a
Aloysius Gilang <gilangpramdya84@gmail.com>
2023-04-05 20:01:06 +0700

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 import { Inter, Open_Sans, Playfair_Display } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div className="font-inter">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
